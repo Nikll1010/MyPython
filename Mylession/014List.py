3eac3110fcb17323c6d84deca285f2bad59166b6
Nikll1010 <294758896@qq.com>
2018-09-07 18:33:03 +0800

    my lession新增

---
+++ Mylession/014List.py
@@ -0,0 +1,21 @@
+a = list()  # 创建一个空列表，输出他的类型
+print(type(a))
+b = list((1, 2, 3, 4, 5, 6, 7, 8))  # list()方法用于将可迭代对象--元组--生成一个列表
+print(b)
+c = list("i love you")  # 将可迭代对象--字符串--生成一个列表（注意空格）
+print(c)
+# tuple()与str() 方法一样，都是将可迭代对象iterable生成相应的对象
+d = tuple()
+print(type(d))
+e = tuple("i love you")
+print(e)
+f = tuple([1, 2, 3, 4, 5, 6, 7])
+print(f)
+'''------'''
+g = str()
+print(type(g))
+h = str([1, 2, 3, 4, 5, 6])
+print(h)
+print(type(h))
+i = str(('i', ' ', 'l', 'o', 'v', 'e', ' ', 'y', 'o', 'u'))
+print(i)
